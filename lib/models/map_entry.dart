c4ce33fde48eb2a70da86541f48bd1d40fa86d4f
Rafi Ahmed <ahmrafi22work@gmail.com>
2025-12-09 13:32:47 +0600

    map view tab

---
+++ lib/models/map_entry.dart
@@ -0,0 +1,43 @@
+class MapEntry {
+  final int id;
+  final String title;
+  final double lat;
+  final double lon;
+  final String? image;
+
+  MapEntry({
+    required this.id,
+    required this.title,
+    required this.lat,
+    required this.lon,
+    this.image,
+  });
+
+  factory MapEntry.fromJson(Map<String, dynamic> json) {
+    // Helper function to safely convert to double
+    double _parseDouble(dynamic value) {
+      if (value == null || value == '') {
+        return 0.0;
+      } else if (value is int) {
+        return value.toDouble();
+      } else if (value is double) {
+        return value;
+      } else if (value is String) {
+        return double.tryParse(value) ?? 0.0;
+      }
+      return 0.0;
+    }
+
+    return MapEntry(
+      id: json['id'] as int,
+      title: json['title'] as String? ?? '',
+      lat: _parseDouble(json['lat']),
+      lon: _parseDouble(json['lon']),
+      image: json['image'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'title': title, 'lat': lat, 'lon': lon, 'image': image};
+  }
+}
